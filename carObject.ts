4d3a111e16e27c6924ff2f5d62f47442057486d5
Yana <Yana>
2024-10-10 18:33:23 +0300

    add: car object

---
+++ carObject.ts
@@ -0,0 +1,12 @@
+const car = {
+    salon: 'темный',
+    securitySystems: 'ABS',
+    pillows: 'В полной комплектации',
+    exterior: 'Рейлинги на крыше',
+    headlights: 'светодиодные',
+    heating: false
+}
+
+car.headlights='гирлянда'
+
+console.log(car)
